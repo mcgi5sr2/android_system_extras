69e9e61f277fd993f6f9bb0c572128595384c735
Muhammed Nazim <mnm9994u@gmail.com>
2013-04-23 14:02:51 +0200

    Overwork SysExtras
    Adds new features and is Supposed to improves system stability

--- ext4_utils/mkuserimg.sh
+++ ext4_utils/mkuserimg.sh
@@ -19,13 +19,11 @@ fi
 
 if [ $# -ne 4 -a $# -ne 5 -a $# -ne 6 ]; then
   usage
-  exit 1
 fi
 
 SRC_DIR=$1
 if [ ! -d $SRC_DIR ]; then
   echo "Can not find directory $SRC_DIR!"
-  exit 2
 fi
 
 OUTPUT_FILE=$2
@@ -36,16 +34,15 @@ FC=$6
 
 case $EXT_VARIANT in
   ext4) ;;
-  *) echo "Only ext4 is supported!"; exit 3 ;;
+  *) echo "Only ext4 is supported!"; ;;
 esac
 
 if [ -z $MOUNT_POINT ]; then
   echo "Mount point is required"
-  exit 2
 fi
 
 if [ -z $SIZE ]; then
-    SIZE=128M
+  SIZE=128M
 fi
 
 if [ -n "$FC" ]; then
@@ -55,6 +52,3 @@ fi
 MAKE_EXT4FS_CMD="make_ext4fs $ENABLE_SPARSE_IMAGE $FCOPT -l $SIZE -a $MOUNT_POINT $OUTPUT_FILE $SRC_DIR"
 echo $MAKE_EXT4FS_CMD
 $MAKE_EXT4FS_CMD
-if [ $? -ne 0 ]; then
-  exit 4
-fi
