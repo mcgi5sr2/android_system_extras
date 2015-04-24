173f5f0e503605a47b387b6e8cbcfd5d3f3a7ee5
Nicholas Flintham <nick@flinny.org>
2015-04-24 13:28:39 +0100

    Revert "ext4_utils: Yet another MMC discard pain in the ass"
    This reverts commit c316d84a8c9e07f5ac115545ce7f52d061ae206a.
    
    Change-Id: I8bf05ec24571da98f091776be42456e331df5871

--- ext4_utils/Android.mk
+++ ext4_utils/Android.mk
@@ -81,13 +81,6 @@ LOCAL_SHARED_LIBRARIES := \
     libselinux \
     libsparse \
     libz
-
-# Some MMCs have VERY slow secure erase - use
-#standard erase instead
-ifeq ($(BOARD_SUPPRESS_SECURE_ERASE),true)
-    LOCAL_CFLAGS += -DSUPPRESS_SECURE_ERASE
-endif
-
 include $(BUILD_SHARED_LIBRARY)
 
 
@@ -97,13 +90,6 @@ LOCAL_MODULE := libext4_utils_static
 LOCAL_STATIC_LIBRARIES += \
     libselinux \
     libsparse_static
-
-# Some MMCs have VERY slow secure erase - use
-#standard erase instead
-ifeq ($(BOARD_SUPPRESS_SECURE_ERASE),true)
-    LOCAL_CFLAGS += -DSUPPRESS_SECURE_ERASE
-endif
-
 include $(BUILD_STATIC_LIBRARY)
 
 
